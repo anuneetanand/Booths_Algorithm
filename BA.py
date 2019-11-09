9f0805b2e1121f1344a9d882200709fef9a86c95
Anuneet Anand <anuneet18022@iiitd.ac.in>
2019-11-09 17:35:47 +0530

    ok

--- BA.py
+++ BA.py
@@ -2,8 +2,6 @@
 # Project-2 : Booth's Algorithm
 # Anuneet Anand (2018022)
 # Pruthwiraj Nanda (2018075)
-import time
-start_time = time.time()
 
 def Int(x):
 	''' Finds Decimal Representation Of A Binary Number'''
@@ -44,6 +42,7 @@ def TC(x):
 	return s
 
 def Num(x):
+	''' Converts Decimal Number Into Its Appropiate Binary Representation'''
 	if x>=0:
 		return Bin(abs(x))
 	else:
@@ -58,44 +57,39 @@ def Add(x,y):
 	c = 0
 	z = ""
 	for i in range(n-1,-1,-1):
-		r = int(x[i])^int(y[i])^c
+		r = int(x[i]) ^ int(y[i]) ^ c
 		c = ( (int(x[i]) ^ int(y[i]) ) & c ) | ( int(x[i]) & int(y[i]) )
 		z = str(r) + z
 	return z
 
 def Multiply(X,Y):
 	''' Multiplication Algorithm '''
+
 	x = Num(X)
 	y = Num(Y)
 	l = max(len(x),len(y))
-	x = Ext(x,l)
-	y = Ext(y,l)
 
-	A = x + "0" * (l+1)
-	S = Ext(Num(-X),l) + "0" * (l+1)
-	P = "0" * (l) + y + "0"
+	A = Ext(x,l) + "0" * (l+1)
+	B = Ext(Num(-X),l) + "0" * (l+1)
+	P = "0" * (l) + Ext(y,l) + "0"
 
 	for i in range(l):
-		if P[-1]=="1" and P[-2]=="0":
+		if P[-1]=="1" and P[-2]=="0":			# Last Two Bits : "01"
 			P = Add(P,A)
-		elif P[-1]=="0" and P[-2]=="1":
-			P = Add(P,S)
-		P = P[0]+P[:-1]
+		elif P[-1]=="0" and P[-2]=="1":			# Last Two Bits : "10"
+			P = Add(P,B)
+		P = P[0]+P[:-1]							# Right Shift
 	P = P[:-1]
-	return Int(P)
+	return Int(P),P
 
 def Divide(X,Y):
 	''' Division Algorithm '''
-	if (Y==0):
-		print("Invalid Input! Divisor Can't Be Zero!")
-		return
+
 	x = Num(X)
 	y = Num(Y)
 	l = max(len(x),len(y))
-	x = Ext(x,l)
-	y = Ext(y,l)
-	qs = int(x[0])^int(y[0])
-	rs = int(x[0])
+	qs = int(x[0]) ^ int(y[0])					# Determining Sign Of Quotient
+	rs = int(x[0])								# Determining Sign Of Remainder
 
 	Q =  Ext(Num(abs(X)),l)
 	R = "0" * (l+1)
@@ -103,11 +97,11 @@ def Divide(X,Y):
 	B =  Ext(Num(-abs(Y)),l)
 
 	for i in range(l):
-		if R[0]=="1":
-			R = R[1:] + Q[0]
+		if R[0]=="1":							# R<0
+			R = R[1:] + Q[0]					# R = R + A
 			R = Add(R,A)
-		else:
-			R = R[1:] + Q[0]
+		else:									# R>=0
+			R = R[1:] + Q[0]					# R = R - A
 			R = Add(R,B)
 		if R[0]=="1":
 			Q = Q[1:] + "0"
@@ -121,39 +115,21 @@ def Divide(X,Y):
 	if rs == 1:
 		R = TC(R)
 
-	return Int(Q),Int(R)
+	return Int(Q),Int(R),Q,R
 
-print("Select An Operation:- ")
-print("1: Multiplication")
-print("2: Division")
-c = input()
-
-for i in range(1,1000):
-	for j in range(1,1000):
-		if (i*j)!=Multiply(i,j):
-			print(i,j)
-		Q,R = Divide(i,j)
-		if (i//j)!=Q or (i%j)!=R:
-			print(i,j)
-print("--- %s seconds ---" % (time.time() - start_time))
-
-if c == "1":
-	print("Enter Multiplicand: ",end="")
-	X = int(input())
-	print("Enter Multiplier: ",end="")
-	Y = int(input())
-	P = Multiply(X,Y)
-	print("Product: ",P)
-
-elif c == "2":
-	print("Enter Dividend: ",end="")
-	X = int(input())
-	print("Enter Divisor: ",end="")
-	Y = int(input())
-	Q,R = Divide(X,Y)
-	print("Quotient: ",Q)
-	print("Remainder: ",R)
-
+print("Enter First Number: ",end="")
+X = int(input())
+print("Enter Second Number: ",end="")
+Y = int(input())
+if (Y==0):
+	print("Divsior Can't Be Zero")
 else:
-	print("Invalid Choice!")
+	P,Pb = Multiply(X,Y)
+	Q,R,Qb,Rb = Divide(X,Y)
 
+	with open('Output.txt','w') as Output_File:
+		Output_File.write("Numbers: "+str(X)+", "+str(Y)+"\n")
+		Output_File.write("Product: "+str(P)+"	"+str(Pb)+"\n")
+		Output_File.write("Quotient: "+str(Q)+"	"+str(Qb)+"		"+"Remainder :"+str(R)+"	"+str(Rb)+"\n")
+		Output_File.close()
+# END OF CODE
